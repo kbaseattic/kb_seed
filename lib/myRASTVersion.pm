641d66b850f52074ad690019682dc51d97aa01df
Robert Olson <olson@ash.mcs.anl.gov>
2012-05-02 15:10:44 -0500

    update to seed 1.014. missed committing the new scripts.

--- lib/myRASTVersion.pm
+++ lib/myRASTVersion.pm
@@ -6,9 +6,9 @@ sub new
 {
     my($class) = @_;
     my $self = {
-	release => "1.013",
-	package_date => 1335986879,
-	package_date_str => "May 02, 2012 14:27:59",
+	release => "1.014",
+	package_date => 1335989382,
+	package_date_str => "May 02, 2012 15:09:42",
     };
     return bless $self, $class;
 }
